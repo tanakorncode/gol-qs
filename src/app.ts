c2fedbb083c8d59f30e85780820c9cf3a9e2d4e6
tanakorncode <tanakorncode@gmail.com>
2024-07-09 17:13:13 +0700

    rebuild

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,26 @@ import { createApp, ref } from "vue"
 import App from "./App.vue"
 import Hello from "./components/Hello.vue"
 
+declare global {
+  interface Window {
+    initGloQuestion: () => void;
+  }
+}
+
 // import { createBootstrap } from "bootstrap-vue-next"
 
-const app = createApp(App)
-// app.use(createBootstrap({ components: true, directives: true }))
-app.component("Hello", Hello)
-app.mount("#app")
+function init() {
+  const app = createApp(App)
+  // app.use(createBootstrap({ components: true, directives: true }))
+  app.component("Hello", Hello)
+  app.mount("#app")
+}
+
+window.initGloQuestion = init;
+
+function webLoad() {
+  console.log("webLoad");
+  window.initGloQuestion()
+}
+
+document.addEventListener("DOMContentLoaded", webLoad, true);
